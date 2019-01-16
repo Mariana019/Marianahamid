31735858d04d1c3c6039cf5c707eaf5fdd9739bf
Mariana019 <46749315+Mariana019@users.noreply.github.com>
2019-01-16 21:45:10 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<table width="337">
+<td width="298" height="300" align="center"> <p><strong> Nama : Mariana </p>
+  <p>Nim  : 215 280 019</p>
+  <p>Phone  :************* </p>
+  <p>sulawesi selatan, Pinrang</p>
+  </strong>
+</table>  
